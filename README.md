e6cc8d67b1684c15cceef73d405c583c5d6e3024
Brad Flaugher <brad@bradflaugher.com>
2023-01-04 15:14:02 -0500

    scheduler v2

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 a tournament scheduler for tennis, squash, racquets and other tournaments, written in python. for use at RCOP.
 
 # TODOs
+* End the tourney earlier, otherwise it always books court A
 * Need to create an example survey (use google forms)
 * Assume 8 teams for normal tourney.
--- create_tourney.py
+++ create_tourney.py
@@ -2,6 +2,10 @@ import os
 import csv
 from pprint import pprint
 
+import datetime
+import pytz
+from dateutil.parser import parse
+
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
@@ -54,12 +58,60 @@ def setup_court_schedules(courts):
     court_schedules = {}
     
     for court in courts:
-        #TODO create dict with court info and special list inside for schedule
+        court_schedules[court['Court Name']] = {
+                'sports' : [x.strip() for x in court['Sports'].split(',')],
+                'open_time' : parse(court['Open Time'],ignoretz=True),
+                'close_time' : parse(court['Close Time'],ignoretz=True),
+                'events' : []
+        }
 
+    print(court_schedules)
+    return court_schedules
 
-def schedule_court(player1, player2, sport, courts, time_restriction=[]):
-    #TODO use time restriction    
-    pass
+def schedule_court(player, opponent, court_schedules, sport, duration, start_dt, end_dt, time_preference=[]):
+    for court_name in court_schedules:
+        if sport in court_schedules[court_name]['sports']:
+            court_open = court_schedules[court_name]['open_time']
+            court_close = court_schedules[court_name]['close_time']
+            events = court_schedules[court_name]['events']
+
+            if 'anytime' in time_preference or 'early' in time_preference:
+                test_start = start_dt
+            elif 'late' in time_preference:
+                test_start = start_dt.replace(hour=17)
+            else:
+                test_start = start_dt
+
+            test_start = test_start
+            test_end = test_start + datetime.timedelta(minutes=duration)
+                       
+            while test_end < end_dt:
+                if len(events) == 0:
+                    if test_start.time() > court_open.time() and test_end.time() < court_close.time():
+                        return { 
+                            'player': player["Player or Team Name"],
+                            'opponent': opponent["Player or Team Name"],
+                            'court_name': court_name,
+                            'match_start' : test_start,
+                            'match_end': test_end
+                            }
+                else:
+                    conflict = False
+                    for event in events:
+                        if not (test_start.time() > court_open.time() and test_end.time() < court_close.time() and test_start < event['match_start'] and test_start < event['match_end'] and test_end < event['match_start'] and test_end < event['match_end']) and not (test_start.time() > court_open.time() and test_end.time() < court_close.time() and test_start > event['match_start'] and test_start > event['match_end'] and test_end > event['match_start'] and test_end > event['match_end']):
+                            conflict = True
+
+                    if conflict == False:
+                        return { 
+                            'player': player["Player or Team Name"],
+                            'opponent': opponent["Player or Team Name"],
+                            'court_name': court_name,
+                            'match_start' : test_start,
+                            'match_end': test_end
+                            }
+                test_start = test_start + datetime.timedelta(minutes=5)
+                test_end = test_end + datetime.timedelta(minutes=5)
+    return { 'court_name': 'failed' }
 
 
 def main():
@@ -67,14 +119,18 @@ def main():
     players = setup_players()
     courts = setup_courts()
     player_schedules = setup_player_schedules(players)    
+    court_schedules = setup_court_schedules(courts)
 
     for sport in sports:
         tourney_title= sport["Sport"] + " Class " + sport["Class"]
         sport_name = sport["Sport"]
         class_name = sport["Class"]
-        length = int(sport["Time"])
+        duration = int(sport["Time"])
         total_byes = int(sport["# Byes"])
         
+        tourney_start = parse(sport['Tournament Start Date'] + ' ' +  sport['Tournament Start Time'],ignoretz=True)
+        tourney_end = parse(sport['Tournament End Date'] + ' ' +  sport['Tournament End Time'],ignoretz=True)
+
         scheduled_byes = 0
         tournament = []
 
@@ -95,7 +151,6 @@ def main():
                 player = players_to_schedule.pop()
                 scheduled_players.append(player)
                 tournament.append({
-                        "Time":"NA",
                         "Player(s)":player["Player or Team Name"],
                         "Opponent(s)": "BYE, due to tournament rules"
                         })
@@ -105,7 +160,6 @@ def main():
             if len(players_to_schedule) <= 0:
                 scheduled_players.append(player)
                 tournament.append({
-                    "Time": player["Available Times"],
                     "Player(s)":player["Player or Team Name"],
                     "Opponent(s)": "BYE, due to someone else's scheduling conflict or odd number of players"
                     })
@@ -117,18 +171,22 @@ def main():
                         opponent_availability = [x.strip() for x in opponent["Available Times"].split(',')]
                         joint_availability = set(player_availability) & set(opponent_availability)
                         if len(joint_availability) > 0:
-                            # TODO add court time here
-                            # TODO check players schedules in other events as well
-                            # TODO pass player schedules and also append to player schedules here 
-                            players_to_schedule.remove(opponent)
-                            scheduled_players.append(opponent)
-                            scheduled_players.append(player)
-                            tournament.append({
-                                    "Time" : str(joint_availability),
+                            court_event = schedule_court(player, opponent, court_schedules, sport_name, duration, tourney_start, tourney_end, joint_availability)
+                            print(court_event)
+                            if 'failed' not in court_event['court_name']:
+                                court_schedules[court_event['court_name']]['events'].append(court_event)
+                                players_to_schedule.remove(opponent)
+                                scheduled_players.append(opponent)
+                                scheduled_players.append(player)
+                                tournament.append({
+                                    "Start" : court_event['match_start'],
+                                    "End": court_event['match_end'],
+                                    "Court" : court_event['court_name'],
                                     "Player(s)" : player["Player or Team Name"],
-                                    "Opponent(s)" : opponent["Player or Team Name"]
+                                    "Opponent(s)" : opponent["Player or Team Name"],
+                                    "Joint Availability" : str(joint_availability)
                                 })
-                            scheduled = True
+                                scheduled = True
                 if not scheduled:
                     abandoned_players.append(player)
         
