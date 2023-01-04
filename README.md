1bfaa40a4f9ab1f314b304ddc9ca67b7f19d0a01
Brad Flaugher <brad@bradflaugher.com>
2023-01-04 12:57:11 -0500

    adding court info

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 a tournament scheduler for tennis, squash, racquets and other tournaments, written in python. for use at RCOP.
 
 # TODOs
-* Different classes have different court use times
-* Courts need to keep track of their own availability
 * Need to create an example survey (use google forms)
 * Assume 8 teams for normal tourney.
--- create_tourney.py
+++ create_tourney.py
@@ -2,96 +2,153 @@ import os
 import csv
 from pprint import pprint
 
-sports = []
-players = []
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-# TODO select these files instead of pick from data folder
-with open(os.path.join('data','sports.csv')) as f:
-    sports = [{k: v for k, v in row.items()}
-        for row in csv.DictReader(f, skipinitialspace=True)]
 
-with open(os.path.join('data','players.csv')) as f:
-     players = [{k: v for k, v in row.items()}
-        for row in csv.DictReader(f, skipinitialspace=True)]
+def setup_courts():
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename(title='Choose courts csv file...') # show an "Open" dialog box and return the path to the selected file
 
-for sport in sports:
-    tourney_title= sport["Sport"] + " Class " + sport["Class"]
-    sport_name = sport["Sport"]
-    class_name = sport["Class"]
-    length = int(sport["Time"])
-    total_byes = int(sport["# Byes"])
+    with open(filename) as f:
+         courts = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
+
+    return courts
+
+def setup_players():
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename(title='Choose players csv file...') # show an "Open" dialog box and return the path to the selected file
+
+    with open(filename) as f:
+         players = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
+
+    return players
+
+def setup_sports():
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename(title='Choose sports csv file...') # show an "Open" dialog box and return the path to the selected file
     
-    scheduled_byes = 0
-    tournament = []
+    with open(filename) as f:
+        sports = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
 
-    sport_players = []
+    return sports
+
+def setup_player_schedules(players):
+    
+    player_schedules={}
+
+    #TODO look for people with the same name
     for player in players:
-        if player["Sport"] == sport_name and player["Class"] == class_name:
-            sport_players.append(player)
-    
-    num_players=len(sport_players)
-    num_players_to_schedule = num_players - total_byes
-    players_to_schedule = sport_players 
-    abandoned_players = []
-    scheduled_players = []
+        individual_names = [x.strip() for x in player['Player or Team Name'].split('+')]
+        for name in individual_names:
+            player_schedules[name]={}
 
-    # TODO schedule byes only for the best players, need handicaps first
-    if total_byes > len(players_to_schedule):
-        for i in range(total_byes):
+    print(player_schedules)
+    return player_schedules
+
+
+def setup_court_schedules(courts):
+    court_schedules = {}
+    
+    for court in courts:
+        #TODO create dict with court info and special list inside for schedule
+
+
+def schedule_court(player1, player2, sport, courts, time_restriction=[]):
+    #TODO use time restriction    
+    pass
+
+
+def main():
+    sports = setup_sports()
+    players = setup_players()
+    courts = setup_courts()
+    player_schedules = setup_player_schedules(players)    
+
+    for sport in sports:
+        tourney_title= sport["Sport"] + " Class " + sport["Class"]
+        sport_name = sport["Sport"]
+        class_name = sport["Class"]
+        length = int(sport["Time"])
+        total_byes = int(sport["# Byes"])
+        
+        scheduled_byes = 0
+        tournament = []
+
+        sport_players = []
+        for player in players:
+            if player["Sport"] == sport_name and player["Class"] == class_name:
+                sport_players.append(player)
+        
+        num_players=len(sport_players)
+        num_players_to_schedule = num_players - total_byes
+        players_to_schedule = sport_players 
+        abandoned_players = []
+        scheduled_players = []
+
+        # TODO schedule byes only for the best players, need handicaps first
+        if total_byes > len(players_to_schedule):
+            for i in range(total_byes):
+                player = players_to_schedule.pop()
+                scheduled_players.append(player)
+                tournament.append({
+                        "Time":"NA",
+                        "Player(s)":player["Player or Team Name"],
+                        "Opponent(s)": "BYE, due to tournament rules"
+                        })
+
+        while len(players_to_schedule) > 0:
             player = players_to_schedule.pop()
-            scheduled_players.append(player)
-            tournament.append({
-                    "Time":"NA",
+            if len(players_to_schedule) <= 0:
+                scheduled_players.append(player)
+                tournament.append({
+                    "Time": player["Available Times"],
                     "Player(s)":player["Player or Team Name"],
-                    "Opponent(s)": "BYE, due to tournament rules"
+                    "Opponent(s)": "BYE, due to someone else's scheduling conflict or odd number of players"
                     })
-
-    while len(players_to_schedule) > 0:
-        player = players_to_schedule.pop()
-        if len(players_to_schedule) <= 0:
-            scheduled_players.append(player)
-            tournament.append({
-                "Time": player["Available Times"],
-                "Player(s)":player["Player or Team Name"],
-                "Opponent(s)": "BYE, due to someone else's scheduling conflict or odd number of players"
-                })
-        else: 
-            scheduled = False
-            for opponent in players_to_schedule:
+            else: 
+                scheduled = False
+                for opponent in players_to_schedule:
+                    if not scheduled:
+                        player_availability = [x.strip() for x in player["Available Times"].split(',')]
+                        opponent_availability = [x.strip() for x in opponent["Available Times"].split(',')]
+                        joint_availability = set(player_availability) & set(opponent_availability)
+                        if len(joint_availability) > 0:
+                            # TODO add court time here
+                            # TODO check players schedules in other events as well
+                            # TODO pass player schedules and also append to player schedules here 
+                            players_to_schedule.remove(opponent)
+                            scheduled_players.append(opponent)
+                            scheduled_players.append(player)
+                            tournament.append({
+                                    "Time" : str(joint_availability),
+                                    "Player(s)" : player["Player or Team Name"],
+                                    "Opponent(s)" : opponent["Player or Team Name"]
+                                })
+                            scheduled = True
                 if not scheduled:
-                    player_availability = [x.strip() for x in player["Available Times"].split(',')]
-                    opponent_availability = [x.strip() for x in opponent["Available Times"].split(',')]
-                    joint_availability = set(player_availability) & set(opponent_availability)
-                    if len(joint_availability) > 0:
-                        # TODO add court time here
-                        # TODO check players schedules in other events as well
-                        players_to_schedule.remove(opponent)
-                        scheduled_players.append(opponent)
-                        scheduled_players.append(player)
-                        tournament.append({
-                                "Time" : str(joint_availability),
-                                "Player(s)" : player["Player or Team Name"],
-                                "Opponent(s)" : opponent["Player or Team Name"]
-                            })
-                        scheduled = True
-            if not scheduled:
-                abandoned_players.append(player)
-    
-    print(tourney_title)
-    print("📅")
-    pprint(tournament)
-    print("🛑")
-    pprint(abandoned_players)
-   
-    # TODO let user select folder to save results to
-    if len(tournament) > 0:
-        with open(os.path.join('data','results',tourney_title+'.csv'),'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=tournament[0].keys())
-            writer.writeheader()
-            writer.writerows(tournament)
-    if len(abandoned_players) > 0:
-        with open(os.path.join('data','results',tourney_title+'ABANDONED.csv'),'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=abandoned_players[0].keys())
-            writer.writeheader()
-            writer.writerows(abandoned_players)
+                    abandoned_players.append(player)
+        
+        print(tourney_title)
+        print("📅")
+        pprint(tournament)
+        print("🛑")
+        pprint(abandoned_players)
+       
+        # TODO let user select folder to save results to
+        if len(tournament) > 0:
+            with open(os.path.join('data','results',tourney_title+'.csv'),'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=tournament[0].keys())
+                writer.writeheader()
+                writer.writerows(tournament)
+        if len(abandoned_players) > 0:
+            with open(os.path.join('data','results',tourney_title+'ABANDONED.csv'),'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=abandoned_players[0].keys())
+                writer.writeheader()
+                writer.writerows(abandoned_players)
 
+if __name__ == "__main__":
+    main()
